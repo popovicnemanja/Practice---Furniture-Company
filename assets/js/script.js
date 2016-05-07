a0015ac8e802400de2461dbe01fd5b2880953dd3
Nemanja Popovic <nemanjaapopovic@gmail.com>
2016-05-07 09:46:19 +0200

    add scroll to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
     $('.menu').on('click', function() {
-        $('nav').find('ul').slideToggle(700)
-    })});
+        $('nav').find('ul').slideToggle(700)});
+
+
+    $(window).scroll(function(){
+    	if($(this).scrollTop() > 100) {
+    		$('.scrollToTop').fadeIn();
+    	} else {
+    		$('.scrollToTop').fadeOut();
+    	}
+    });
+
+    $('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop: 0}, 800);
+		return false;
+		});
+});
+
+
     
